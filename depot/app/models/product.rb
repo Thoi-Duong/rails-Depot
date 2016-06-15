a2fff9823856b05e2b887d11088d3b027694ce12
Thoi Duong <dnhthoi@gmail.com>
2016-06-15 10:51:37 +0700

    complete ordring items

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  has_many :orders, through: :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -6,4 +9,15 @@ class Product < ActiveRecord::Base
     with: /\w*\.png|.jpg|.gif|.jpeg/,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'Line Items present')
+      false
+    end
+  end
 end
